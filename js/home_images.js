a934abcb271a51f7f3570caced871d0f8c616620
user.email <endrita.vllasaliu@student.uni-pr.edu>
2022-01-12 01:02:49 +0100

    INT-44: Modify contact, home, home_image, shop, shopdetail

--- js/home_images.js
+++ js/home_images.js
@@ -3,6 +3,11 @@ var dragSrcEl = null;
 
 window.onload = function () {
   setUpEventListeners();
+  setInterval(() => {
+    if (param >= elements.length) param = 0;
+    setValues(param);
+    param++;
+  }, 3000);
 };
 
 const drag_element = document.getElementById("drag_elements_info");
@@ -70,4 +75,35 @@ function colDragEnd(e) {
   cols.forEach(function (col) {
     col.classList.remove("over");
   });
+}
+const id = document.getElementById("image_title");
+let param = +document.getElementById("image_number").value;
+
+const elements = [
+  {
+    title: "Front end developer",
+    alt: "Front end developer",
+    source: "images/portret1.jpg",
+  },
+  {
+    title: "Back end developer",
+    alt: "Back end developer",
+    source: "images/portret2.jpg",
+  },
+  {
+    title: "3D artist",
+    alt: "3D artist",
+    source: "images/portret3.jpg",
+  },
+  {
+    title: "Team",
+    alt: "Team",
+    source: "images/teamwork.jpg",
+  },
+];
+
+function setValues(param = 0) {
+  document.getElementById("image_title").innerHTML = elements[param].title;
+  document.getElementById("image").src = elements[param].source;
+  document.getElementById("image").alt = elements[param].alt;
 }
